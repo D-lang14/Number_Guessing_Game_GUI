d7d5c76201526aee1bf4037f02b4a5e7c31edb0f
Disha Patil <dishapatil2002@gmail.com>
2022-01-13 16:14:29 +0530

    Successfully Modified the Program

--- 1. Number Guessing/ModifiedVersion.py
+++ 1. Number Guessing/ModifiedVersion.py
@@ -1,39 +1,64 @@
 # ----------------------------------------------------Modified version of Number Guessing Game----------------------------------------------------
 
 import random
+import math
 
-# Range is given by user
-rng1 = int(input("Enter your first desired range: "))
-rng2 = int(input("Enter your last desired range: "))
+# Defining a funtion
+def numberGuessingGame ():
 
-# Generating random numbers between the range given by user
-randm = random.randint(rng1, rng2)
+    # Handling errors
+    try:
+        # Range is given by user
+        rng1 = int(input("Enter lower bound: "))
+        rng2 = int(input("Enter upper bound: "))
 
-# Initialization
-count = 0
+        # Generating random numbers between the range given by user
+        randm = random.randint(rng1, rng2)
+        print("\n\tYou've only ", round(math.log(rng2 - rng1 + 1, 2)), " chances to guess the integer!\n")
 
-# chances of guessing no. restricted to 5 times
-while count < 5:
+        # Initialization
+        count = 0
 
-    # Counting guesses
-    count += 1
+        # chances of guessing no. restricted to 5 times
+        while count < math.log(rng2 - rng1 + 1, 2):
 
-    print('You Have 5 Chances To Guess')
+            # Counting guesses
+            count += 1
 
-    # Taking input of guess
-    guess = int(input("Enter your guess: "))
-    if randm == guess:
-        print("congratulations! You took", count, "atempts")
-        break
+            # Taking input of guess
+            guess = int(input("Enter your guess: "))
+            
 
-    # Giving hints to the user
-    elif randm < guess:
-        print("Your guess is too high!")
+            if randm == guess:
+                print("\n\tCongratulations! You took", count, "atempts")
+                # Once guess is correct then it should break here
+                exit(0)
 
-    elif randm > guess:
-        print("Your guess is too low!")
+            # Giving hints to the user
+            elif randm < guess:
+                print("\tYour guess is too high!")
 
-    else:
-        print("try again")
+            elif randm > guess:
+                print("\tYour guess is too low!")
+
+            else:
+                print("Try Again")
+                
+            # If Guessing is more than required guesses
+            if count >= math.log(rng2 - rng1 + 1, 2):
+                print("\nThe number is %d" % randm)
+                print("\tBetter Luck Next time!")
+                exit(0)
+
+        # This handles the empty input error
+        int('')
+    # error name
+    except ValueError:
+        print("\n Please Enter Proper Value \n")
+        numberGuessingGame()
+        
+if __name__ == "__main__":
+    # calling function
+    numberGuessingGame()
 
 # ----------------------------------------------------End of Program----------------------------------------------------
